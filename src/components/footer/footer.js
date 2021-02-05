88245ea08201c47a7a4cafaca67d23773ba18565
afgcsborges <68897001+afgcsborges@users.noreply.github.com>
2021-02-05 14:31:20 +0000

    added project

---
+++ src/components/footer/footer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+export default function Footer({ resumeData }) {
+  return (
+    <footer>
+      <div className="row">
+        <div className="twelve columns">
+          <ul className="social-links">
+            {
+              resumeData.socialLinks && resumeData.socialLinks.map((item, index) => {
+                return (
+                  <li key={index}>
+                    <a href={item.url}>
+                      <i className={item.className} />
+                    </a>
+                  </li>
+                )
+              })
+            }
+          </ul>
+          <ul className="copyright">
+            <li>António Francisco Borges</li>
+            <li>afgcsborges@gmail.com</li>
+          </ul>
+        </div>
+        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
+      </div>
+    </footer>
+  );
+}
---
+++ src/resumeData.js
@@ -0,0 +1,114 @@
+let resumeData = {
+  "imagebaseurl":"https://techonoeticsclub.github.io/",
+  "name": "Francisco Borges",
+  "role": "Junior Full-Stack Developer",
+  "linkedinId":"afrancisco-borges",
+  "skypeid": "Your skypeid",
+  "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/afrancisco-borges/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"http://github.com/afgcsborges",
+        "className":"fa fa-github"
+      },
+    ],
+  "aboutme":"I have finished my B.E. in Computer Science and have been working in industry for past few years.",
+  "address":"India",
+  "website":"https://www.coderwhodreams.com",
+  "education":[
+    {
+      "UniversityName":"Mumbai University",
+      "specialization":"B.E. CS",
+      "MonthOfPassing":"Jan",
+      "YearOfPassing":"2015",
+      "Achievements":"Finished internship at Perisitent Systems in Final Year"
+    },
+    {
+      "UniversityName":"GreyAtom",
+      "specialization":"Data science",
+      "MonthOfPassing":"Jan",
+      "YearOfPassing":"2019",
+      "Achievements":"Won hackathon organised by GreyAtom Team"
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"CF",
+      "specialization":"Frontend",
+      "MonthOfLeaving":"Jan",
+      "YearOfLeaving":"2020",
+      "Achievements":"Worked on Product Website"
+    }
+  ],
+  "skillsDescription":"Your skills here",
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"Reactjs"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"Collge ID Card Generator",
+      "description":"Online ID Card Generator using Javascript",
+      "imgurl":"https://res.cloudinary.com/dk22rcdch/image/upload/v1602558431/Blogimages/sol2_jonk5k.gif"
+    },
+    {
+      "name":"Clicker app",
+      "description":"Clicker app using react.js",
+      "imgurl":"https://res.cloudinary.com/dk22rcdch/image/upload/v1602056241/Blogimages/Clicker_o7hqyq.gif"
+    },
+    {
+      "name":"project3",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project2.png"
+    },
+    {
+      "name":"project4",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    },
+    {
+      "name":"project1",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    },
+    {
+      "name":"project2",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project.jpg"
+    },
+    {
+      "name":"project3",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project2.png"
+    },
+    {
+      "name":"project4",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    },
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
